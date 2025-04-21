56275ea10d262bf25facd0be94bf24cd91e491f2
haipham22 <contact@haipham.net>
2025-04-21 15:22:49 +0700

    refactor: improve local-build script structure and add usage instructions

--- local-build.sh
+++ local-build.sh
@@ -1,2 +1,13 @@
 #!/usr/bin/env sh
-docker buildx build --build-arg PHP_FPM_VERSION=${PHP_VERSION}-fpm-alpine --platform linux/amd64,linux/arm64 . -t haipham22/nginx-php-fpm:${PHP_VERSION}-fpm-alpine --push
+
+PHP_VERSION=$1
+if [ -z "$PHP_VERSION" ]; then
+  echo "Usage: $0 <php-version>"
+  exit 1
+fi
+
+docker buildx build \
+    --build-arg  PHP_FPM_VERSION=${PHP_VERSION}-fpm-alpine \
+    --platform linux/amd64,linux/arm64 .  \
+    -t haipham22/nginx-php-fpm:${PHP_VERSION}-fpm-alpine \
+    --push
